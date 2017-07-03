22390f98f03f8a5a347312d8001bcf3f411b5280
Taslot <jluis.larav@gmail.com>
2017-07-03 15:29:50 -0500

    Initial Commit

---
+++ JustDoIt2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JustDoIt2.0
+//
+//  Created by JoseLuis Lara VIllanueva on 7/3/17.
+//  Copyright © 2017 Pachuca Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
